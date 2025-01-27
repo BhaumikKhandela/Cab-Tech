292e75eb1ef83f47164d31a9ec83ec7b4dcb61e7
Kr_Himanshu <155032817+KrHimanshu18@users.noreply.github.com>
2025-01-27 23:49:43 +0530

    schema update
    platform field is not needed in rides table

---
+++ prisma/migrations/20250127181851_rides_update/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `platform` on the `Rides` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Rides" DROP COLUMN "platform";
